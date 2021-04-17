689e3312dfd46cc1fa03c89823a6d2b37ddb986d
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-04-17 17:18:07 +0300

    stelios-v1: hello source tree comment was added

--- recipes/src/pages/index.js
+++ recipes/src/pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <h1> Home Page </h1>
       <h2>Git testing</h2>
       <h3>Git learning branching</h3>
+      <h2>hello source tree</h2>
     </Layout>
   )
 }
