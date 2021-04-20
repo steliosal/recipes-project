df9afa75000b1577a7990e05ad00e16a271d225a
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-04-20 22:11:59 +0300

    styled components plugin

--- recipes/src/pages/index.js
+++ recipes/src/pages/index.js
@@ -5,7 +5,9 @@ import Layout from "../components/Layout"
 export default function Home() {
   return (
     <Layout>
+      <div className = 'home-heading'>
       <h1> Home Page </h1>
+      </div>
     </Layout>
   )
 }
