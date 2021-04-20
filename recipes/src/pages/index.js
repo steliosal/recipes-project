06c14ce250ebb05829288300fed4d9021ecfe0fe
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-04-20 20:33:42 +0300

    random

--- recipes/src/pages/index.js
+++ recipes/src/pages/index.js
@@ -6,12 +6,6 @@ export default function Home() {
   return (
     <Layout>
       <h1> Home Page </h1>
-      <h2>Git testing</h2>
-      <h3>getting better and better</h3>
-      <h2>hello source tree</h2>
-      <h2>git ignore learing</h2>
-      <h2>I want to learn about branches</h2>
-      <h2>YO YO YO</h2>
     </Layout>
   )
 }
