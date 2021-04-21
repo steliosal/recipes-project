e19c037ee9cde8cdbf3ee3436ccd7a173489e45a
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-04-21 22:33:55 +0300

    navbar progress

--- recipes/src/components/NavBar.js
+++ recipes/src/components/NavBar.js
@@ -1,29 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
 import { FiAlignJustify } from "react-icons/fi"
+import logo from "../assets/images/logo.svg"
 
 const NavBar = () => {
   return (
-    <nav>
-      <FiAlignJustify size="2rem" />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/tags">Tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">Recipes</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    (
+    <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="simply recipes"/>
+            </Link>
+            <button className="nav-btn">
+              <FiAlignJustify/>
+            </button>
+          </div>
+          <div className="nav-links show-links">
+            <Link to="/"className="nav-link" 
+            activeClassName="active-link">
+              Home</Link>
+            <Link to="/recipes"className="nav-link" 
+            activeClassName="active-link">
+              Recipes</Link>
+            <Link to="/tags"className="nav-link" 
+            activeClassName="active-link">
+              Tags</Link>
+            <Link to="/about"className="nav-link" 
+            activeClassName="active-link">
+              About</Link>
+              <div className="nav-link contact-link">
+                <Link to='/contact' className='btn'>
+                  Contact
+                </Link>
+              </div>
+          </div>
+        </div>
+      </nav>
+  )
   )
 }
 
