1b9385bf1de7af1bac3238b805ed421ae4dab36e
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-04-22 15:54:41 +0300

    navbar state - image plugin installation

--- recipes/src/components/NavBar.js
+++ recipes/src/components/NavBar.js
@@ -1,43 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { FiAlignJustify } from "react-icons/fi"
 import logo from "../assets/images/logo.svg"
 
 const NavBar = () => {
+  const [show, setShow] = useState(false)
+
   return (
-    (
     <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <Link to="/">
-              <img src={logo} alt="simply recipes"/>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simply recipes" />
+          </Link>
+          <button className="nav-btn" onClick={()=> {setShow(!show)}}>
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className={show ?'nav-links show-links': 'nav-links'}>
+          <Link to="/" className="nav-link" activeClassName="active-link" onClick={()=> {setShow(false)}}>
+            Home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={()=> {setShow(false)}}
+          >
+            Recipes
+          </Link>
+          <Link to="/tags" 
+          className="nav-link" 
+          activeClassName="active-link"
+          onClick={()=> {setShow(false)}}
+          >
+            Tags
+          </Link>
+          <Link to="/about" 
+          className="nav-link" 
+          activeClassName="active-link"
+          onClick={()=> {setShow(false)}}>
+            About
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              Contact
             </Link>
-            <button className="nav-btn">
-              <FiAlignJustify/>
-            </button>
-          </div>
-          <div className="nav-links show-links">
-            <Link to="/"className="nav-link" 
-            activeClassName="active-link">
-              Home</Link>
-            <Link to="/recipes"className="nav-link" 
-            activeClassName="active-link">
-              Recipes</Link>
-            <Link to="/tags"className="nav-link" 
-            activeClassName="active-link">
-              Tags</Link>
-            <Link to="/about"className="nav-link" 
-            activeClassName="active-link">
-              About</Link>
-              <div className="nav-link contact-link">
-                <Link to='/contact' className='btn'>
-                  Contact
-                </Link>
-              </div>
           </div>
         </div>
-      </nav>
-  )
+      </div>
+    </nav>
   )
 }
 
