81230502337af24479834bdd83542243d52b983c
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-04-23 17:45:02 +0300

    added metadata

--- recipes/gatsby-config.js
+++ recipes/gatsby-config.js
@@ -1,15 +1,21 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "@johnsmilga",
+    person: { name: "john", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "susan", age: 21 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-  ],
+    
+  ]
 }
--- recipes/src/pages/index.js
+++ recipes/src/pages/index.js
@@ -5,10 +5,23 @@ import { StaticImage } from "gatsby-plugin-image"
 export default function Home() {
   return (
     <Layout>
-     
-        <StaticImage />
-        <h1> Home Page </h1>
-      
+      <main className="page">
+        <header className="hero">
+      <StaticImage 
+      src='../assets/images/main.jpeg' 
+      alt='eggs'
+      className='hero-img'
+      placeholder='blurred'
+      layout='fullWidth' />
+      <div className="hero-container">
+        <div className="hero-text">
+          <h1>Simply Recipes</h1>
+          <h4>no fluff, just recipes</h4>
+        </div>
+      </div>
+          
+        </header>
+      </main>
     </Layout>
   )
 }
