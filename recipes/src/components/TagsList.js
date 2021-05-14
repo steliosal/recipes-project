cf975b965cd124eab74b9be194e7f8482c5a2342
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-05-14 09:50:55 +0300

    tags

--- recipes/src/components/TagsList.js
+++ recipes/src/components/TagsList.js
@@ -1,7 +1,25 @@
 import React from "react"
+import setupTags from "../utils/setupTags"
+import { Link } from "gatsby"
 
-const TagsList = () => {
-  return <div>this is tags List</div>
+const TagsList = ({ recipes }) => {
+  const newTags = setupTags(recipes)
+
+  return (
+    <div className="tag-container">
+      <h4>Recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag
+          return (
+            <Link key={index} to="{`/&{text}`}">
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TagsList
--- recipes/src/pages/contact.js
+++ recipes/src/pages/contact.js
@@ -1,7 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import RecipesList from "../components/RecipesList"
+import { graphql } from "gatsby"
 
-const Contact = () => {
+
+const Contact = ({ data }) => {
+  const recipes = data.allContentfulRecipe.nodes
   return (
     <Layout>
       <main className="page">
@@ -43,9 +47,33 @@ const Contact = () => {
             </form>
           </article>
         </section>
+        <section className = 'featured-recipes'>
+          <RecipesList recipes={recipes} />
+        </section>
       </main>
     </Layout>
   )
 }
+
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
+
 
 export default Contact
